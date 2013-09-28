4d221481f9f4a8a69ccae67d2adb1f00ca8f51ec
Mohammad Elmi <elmi.moh@gmail.com>
2013-09-28 21:27:39 +0330

    pressure coefficient matrix generation + boundaries completed

--- src/pressure.h
+++ src/pressure.h
@@ -20,15 +20,32 @@ class pressure
     typedef Eigen::Triplet<double> triplet;
     typedef std::vector<triplet> triplet_vector;
 
-    static void add_pressure_row(size_t n, size_t *row_idxs, double dx, triplet_vector *v)
+    static void apply_pressure_row_bc(size_t ix0 , size_t ix1 , size_t ix2 , size_t ix3 , triplet_vector *v, bcond bc, double coeff)
+    {
+        //0
+        v->push_back(triplet(ix0, ix0, -4.0 * coeff));
+        v->push_back(triplet(ix0, ix1, +1.0 * coeff));
+        v->push_back(triplet(ix0, ix2, +1.0 * coeff));
+        if (bc.type == bctype::neumann) v->push_back(triplet(ix0, ix0, +2.0 * coeff));
+
+        //1
+        v->push_back(triplet(ix1, ix0, +1.0 * coeff));
+        if (bc.type != bctype::neumann) v->push_back(triplet(ix1, ix1, +2.0 * coeff));
+        v->push_back(triplet(ix1, ix3, -1.0 * coeff));
+    }
+
+    static void add_pressure_row(size_t n, size_t *row_idxs, triplet_vector *v, bcond startbc, bcond endbc, double coeff)
     {
         for (size_t i = 2; i < n - 2; ++i)
         {
             size_t ix = row_idxs[i];
-            v->push_back(triplet(ix, row_idxs[i - 2], 1.0));
-            v->push_back(triplet(ix, ix, 2.0));
-            v->push_back(triplet(ix, row_idxs[i + 2], 1.0));
+            v->push_back(triplet(ix, row_idxs[i - 2], 1.0 * coeff));
+            v->push_back(triplet(ix, ix, -2.0 * coeff));
+            v->push_back(triplet(ix, row_idxs[i + 2], 1.0 * coeff));
         }
+
+        apply_pressure_row_bc(row_idxs[0], row_idxs[1], row_idxs[2], row_idxs[3], v, startbc, coeff);
+        apply_pressure_row_bc(row_idxs[n - 1], row_idxs[n - 2], row_idxs[n - 3], row_idxs[n - 4], v, endbc, coeff);
     }
 
 public:
@@ -36,6 +53,7 @@ public:
 
     sparse_matrix *make_pressure_matrix(domain *d)
     {
+        double coeff = 1.0 / (4.0 * d->delta * d->delta);
         triplet_vector v(d->n * 5);
 
         for (size_t idir = 0; idir < 3; ++idir)
@@ -43,7 +61,9 @@ public:
             {
                 mesh_row *row = d->rows[idir] + irow;
                 size_t *row_idxs = d->get_row_idxs(row);
-                add_pressure_row(row->n, row_idxs, d->delta, &v);
+                bcond start_bc = d->boundaries[row->start_bc].pbc;
+                bcond end_bc = d->boundaries[row->end_bc].pbc;
+                add_pressure_row(row->n, row_idxs, &v, start_bc, end_bc, coeff);
                 delete[] row_idxs;
             }
 
