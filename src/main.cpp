9425497a248e3e2ca4d0590b5dedb8402edb4949
Mohammad Elmi <elmi.moh@gmail.com>
2014-05-21 07:07:16 +0430

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -55,22 +55,22 @@ int main(int argc, char const *argv[])
 {
     cout << "Reading mesh" << endl;
     domain *d = domain::create_from_file("mesh/cavity100x100.json");
-    square_2d(d);
-    check_continuety(d);
-    vof vofc(d);
-    d->write_vtk("out/test0.vtk");
-    for (int i = 0; i < 628 / d->dt; ++i)
-    {
-        cout << "timestep " << i + 1 << endl;
-        vofc.advect();
-        d->write_vtk("out/test" + to_string(i + 1) + ".vtk");
-    }
+    // square_2d(d);
+    // check_continuety(d);
+    // vof vofc(d);
+    // d->write_vtk("out/test0.vtk");
+    // for (int i = 0; i < 628 / d->dt; ++i)
+    // {
+    //     cout << "timestep " << i + 1 << endl;
+    //     vofc.advect();
+    //     d->write_vtk("out/test" + to_string(i + 1) + ".vtk");
+    // }
     // d->write_vtk("out/test1.vtk");
-    cout << "done" << endl;
 
-    // solver s(d, "out/out");
+    solver s(d, "out/out");
     // s.step();
-    // s.run(10);
+    s.run(10);
 
+    cout << "done" << endl;
     return 0;
 }
