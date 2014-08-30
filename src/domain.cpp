64624eeadd9c01024fa5353f1b42e884923ebc07
Mohammad Elmi <elmi.moh@gmail.com>
2014-08-30 18:08:26 +0430

    no need to write ustar and nb

--- src/domain.cpp
+++ src/domain.cpp
@@ -55,13 +55,13 @@ void domain::register_vars()
 {
     varlist.push_back(varinfo("p", 1, true, &p));
     varlist.push_back(varinfo("u", 2, true, &u));
-    varlist.push_back(varinfo("ustar", 2, true, &ustar));
+    varlist.push_back(varinfo("ustar", 2, false, &ustar));
     varlist.push_back(varinfo("rho", 1, false, &rho));
     varlist.push_back(varinfo("nu", 1, false, &nu));
     varlist.push_back(varinfo("uf", 2, false, &uf));
 
     varlist.push_back(varinfo("vof", 1, true, &vof));
-    varlist.push_back(varinfo("nb", 2, true, &nb));
+    varlist.push_back(varinfo("nb", 2, false, &nb));
 }
 
 void domain::write_vtk(std::string file_name)
