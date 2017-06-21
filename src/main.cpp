4746a2f4890195272142c1cc1cc9e8cb6a226cbd
Mohammad Elmi <elmi.moh@gmail.com>
2017-06-21 10:21:57 +0430

    dambreak testcase in progress

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,24 @@ using namespace aban2;
 
 int main(int argc, char const *argv[])
 {
-    // domain *d = domain::create_from_file("mesh/dambreak.json");
-    // std::fill_n(d->vof, d->n, 0.0);
-    // // std::iota(d->no, d->no + d->n, 0);
-    // rectangel(d, {0.1, 0.2, 0}, {0.2, 0.4, 0}, 1);
-    // // rectangel(d, {0.5, 0.5, 0}, {5, 0.6, 0}, 1);
-    // for (size_t i = 0; i < d->n; ++i)
-    // {
-    //     d->rho[i] = d->rho_bar(d->vof[i]);
-    //     d->nu[i] = d->nu_bar(d->vof[i]);
-    // }
-    //
-    // std::cout << "before run" << std::endl << std::flush;
-    // solver *s = new solver(d, "out/dmbrk");
-    // s->run(200);
-    //
-    // delete s;
-    // delete d;
+    domain_t *d = domain_t::create_from_file("mesh/dambreak.json");
+    std::fill_n(d->vof, d->n, 0.0);
+    // std::iota(d->no, d->no + d->n, 0);
+    rectangel_by_bounds(d, {0, 0, 0}, {0.05715, 0.05715, 0}, 1);
+    for (size_t i = 0; i < d->n; ++i)
+    {
+        d->rho[i] = d->rho_bar(d->vof[i]);
+        d->nu[i] = d->nu_bar(d->vof[i]);
+    }
 
-    consistent_vof_test();
+    std::cout << "before run" << std::endl << std::flush;
+    solver *s = new solver(d, "out/dmbrk");
+    s->run(3000);
+
+    delete s;
+    delete d;
+
+    // consistent_vof_test();
     // zalesak_disk_rotation_test();
 
     // if (false)
